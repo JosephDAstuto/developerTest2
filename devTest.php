0b0bb8a33dd267b213606b440fc6342adcf35567
Joseph <n9726136@qut.edu.au>
2019-08-12 21:19:42 +1000

    upgrade and cleaning up

--- devTest.php
+++ devTest.php
@@ -5,7 +5,7 @@
 $colours = ['Blue', 'Red', 'Green', 'Yellow'];
 
 function firstColour($title, $colourArray) {
-    //echo $title;
+
     $lowCol = "";
     $lowPos = 50;
 
@@ -15,7 +15,7 @@ function firstColour($title, $colourArray) {
         if ($loc == false) {
             //do nothing
         } else {
-            // echo "'$colour' exists at '$loc' with previous pos $lowPos \n";
+
             if ($loc < $lowPos){
                 $lowCol = $colour;
                 $lowPos = $loc;
@@ -30,17 +30,15 @@ function firstColour($title, $colourArray) {
 
 $moviesArray=array();
 
-// $tCol = "Red White & Blue";
+//for testing purposes and and sanity checking for dev note the space in the begining
+// $tCol = " Red White & Blue";
 // print_r(firstColour($tCol, $colours));
 
 foreach ($colours as $colour) {
     $url = file_get_contents("http://www.omdbapi.com/?apikey=b5f9ff72&type=movie&s=".$colour."&page=1");
+    //Below for testing multiple colours
     // $url = file_get_contents("http://www.omdbapi.com/?apikey=b5f9ff72&type=movie&s=blue+red");
     $json = json_decode($url, true);
-
-    //print_r($json);
-
-
 
     foreach ($json['Search'] as $movies) {   
         
@@ -50,47 +48,38 @@ foreach ($colours as $colour) {
         array_push($moviesArray, $movies );
     };
 
-
-    // print_r($moviesArray);
-
-    // $movie_title = $json['Rated'];
-
-    // echo $movie_title;
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Colourful Movies</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    
     <h1>Colourful Movies</h1>
 </head>
 <body>
-<table id="movieTable">
-    <tr>
-        <th>Colour</th>
-        <th>Movie Title</th>
-        <th>Release</th>
-        <th>Runtime</th>
-    </tr>
-    <?php
-        
-        foreach ($moviesArray as $movie){
-            //$movieUrl = file_get_contents("http://www.omdbapi.com/?apikey=b5f9ff72&i=".$movie['imdbID']."&type=movie");
-            //$movieJson = json_decode($movieUrl, true);
-            //print_r($movieJson);
-            echo '<tr><td bgcolor="'.$movie['colours'].'">'. $movie['colours'] .'</td> <td>'.$movie['Title'] .'</td>'; /* <td>'.$movieJson['Released'] .'</td><td>'.$movieJson['Runtime'] .'</td></tr>'; */
-        };
-    ?>
-</table>
-    
+    <div class="main">
+        <table id="movieTable">
+            <tr>
+                <th>Colour</th>
+                <th>Movie Title</th>
+                <th>Release</th>
+                <th>Runtime</th>
+            </tr>
+            <?php
+                foreach ($moviesArray as $movie){
+                    $movieUrl = file_get_contents("http://www.omdbapi.com/?apikey=b5f9ff72&i=".$movie['imdbID']."&type=movie");
+                    $movieJson = json_decode($movieUrl, true);
+                    echo '<tr><td bgcolor="'.$movie['colours'].'">'.$movie['colours'].'</td> <td>'.$movie['Title'].'</td><td>'.$movieJson['Released'].'</td><td>'.$movieJson['Runtime'].'</td></tr>'; 
+                };
+            ?>
+        </table>`
+    </div>
 </body>
 </html>
 
